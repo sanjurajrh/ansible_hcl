88c0b6c6f8ccea0e6142146643c08459bde7c784
Student User <student@workstation.lab.example.com>
2024-11-06 00:34:41 -0500

    sample files added

---
+++ projectdir/collections/ansible_collections/azure/azcollection/tests/integration/targets/inventory_azure/runme.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# make sure inventory is empty at the begining of the tests
+ansible-playbook playbooks/empty_inventory_config.yml "$@"
+
+# create vm
+ansible-playbook playbooks/setup.yml "$@"
+
+export ANSIBLE_INVENTORY=test.azure_rm.yml
+
+# using fully qualified name
+ansible-playbook playbooks/create_inventory_config.yml "$@"  
+ansible-playbook playbooks/test_inventory.yml "$@"
+
+# using short name
+ansible-playbook playbooks/empty_inventory_config.yml "$@"
+ansible-playbook playbooks/create_inventory_config.yml "$@"  --extra-vars "template=basic2.yml"
+ansible-playbook playbooks/test_inventory.yml "$@"
+
+# using host filters
+ansible-playbook playbooks/empty_inventory_config.yml "$@"
+ansible-playbook playbooks/create_inventory_config.yml "$@"  --extra-vars "template=filter.yml"
+ansible-playbook playbooks/test_inventory_filter.yml "$@"
+
+
+# teardown
+ansible-playbook playbooks/teardown.yml "$@"
